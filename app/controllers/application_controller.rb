0c2ddd70c0b1264fd5eef08d6e8887c891408272
zootaka <69644312+zootaka@users.noreply.github.com>
2020-11-03 17:40:37 +0900

    ユーザー登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :last_name_kana, :first_name_kana, :nickname, :birth_date])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  has_many :items
+  has_many :comments
+  has_many :purchases
+
+end
