b416a4e2213162f5e1d892dcc600022a39507c56
zootaka <69644312+zootaka@users.noreply.github.com>
2020-11-06 15:44:37 +0900

    削除機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
